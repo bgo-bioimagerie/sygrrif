d6944e581d10bf8faa0a80b3e9294d073d06da72
Sylvain Prigent <sylvain.prigent@univ-rennes1.fr>
2015-10-03 16:07:52 +0200

    test the shortcut

--- Modules/layout.php
+++ Modules/layout.php
@@ -3,30 +3,12 @@
     <head>
         <meta charset="UTF-8" />
         <base href="<?php echo  $rootWeb ?>" >
-        <!-- <link rel="stylesheet" href="Themes/style.css" />  -->
         <title><?php echo  $title ?></title>
     </head>
 
     <body>
-     <!-- 
-        <div id="global">
-        
-            <header>
-                <a href=""><h1 id="titreBlog">Mon Blog</h1></a>
-                <p>Je vous souhaite la bienvenue sur ce modeste blog.</p>
-            </header>
-        -->
             <div id="contenu">
                 <?php echo  $content ?>
-            </div> <!-- #contenu -->
-         <!-- 
-            <footer id="piedBlog">
-                Blog réalisé avec PHP, HTML5 et CSS.
-            </footer>
-            
-        </div> 
-     -->    
+            </div>
     </body>
-
-     
 </html>
