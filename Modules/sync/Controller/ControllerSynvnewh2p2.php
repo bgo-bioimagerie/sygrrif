97d9c20817f3bf7ea0838dc12c5407c80537acca
Sylvain Prigent <sylvain.prigent@univ-rennes1.fr>
2016-01-25 17:52:32 +0100

    edit sync controllers

--- Modules/sync/Controller/ControllerSynvnewh2p2.php
+++ Modules/sync/Controller/ControllerSynvnewh2p2.php
@@ -30,7 +30,6 @@ class ControllerSynvnew extends Controller {
 	// affiche la liste des Sources
 	public function index() {
 
-		/*
 		// 1- install data  base
 		echo "<p> Install core database...  </p>";
 		$installModel = new CoreInitDatabase();
@@ -102,18 +101,127 @@ class ControllerSynvnew extends Controller {
 			$modelUnit->editUnit($unitInfo["id"], $unitInfo["name"], $unitInfo["address"], $up["id_pricing"]+1);
 		}
 		
-		// 5- set all visas to default
-		echo "<p> set all visas to default... </p>";
+		// 5- create visas
+		$modelVisa = new SyVisa();
+		
+		$id_Analyse_images = 1;
+		$id_Cryostat = 2;
+		$id_Inclusion = 3;
+		$id_Loupe_binoculaire = 4;
+		$id_Microscope_nikon_NI_E = 6;
+		$id_Microtome = 7;
+		$id_Scanner_de_lames = 8;
+		$id_Snapfrost = 9;
+		$id_Vibratome = 10;
+		$id_Microscope_80i = 12;
+		$id_Intavis = 14;
+		
+		$id_visa_AF = 4; 
+		$id_visa_MS = 7;
+		$id_visa_PB = 8;
+		$id_visa_RV = 11;
+		$id_visa_GB = 15;
+		
+		$id_AF = 140;
+		$id_MS = 113;
+		$id_PB = 202;
+		$id_RV = 201;
+		$id_GB = 1; // todo add gaelle to users
+		
+		$status_resp = 2;
+		$status_instructor = 1;
+		
+		// create AF visas
+		$modelVisa->addVisa($id_Analyse_images, $id_AF, $status_instructor); // 1
+		$modelVisa->addVisa($id_Cryostat, $id_AF, $status_instructor); // 2
+		$modelVisa->addVisa($id_Inclusion, $id_AF, $status_instructor); // 3
+		$modelVisa->addVisa($id_Loupe_binoculaire, $id_AF, $status_instructor); // 4
+		$modelVisa->addVisa($id_Microscope_nikon_NI_E, $id_AF, $status_instructor); // 5
+		$modelVisa->addVisa($id_Microtome, $id_AF, $status_instructor); // 6
+		$modelVisa->addVisa($id_Scanner_de_lames, $id_AF, $status_instructor); // 7
+		$modelVisa->addVisa($id_Snapfrost, $id_AF, $status_instructor); // 8
+		$modelVisa->addVisa($id_Vibratome, $id_AF, $status_instructor); // 9
+		$modelVisa->addVisa($id_Microscope_80i, $id_AF, $status_instructor); // 10
+		$modelVisa->addVisa($id_Intavis, $id_AF, $status_instructor); // 11
+		
+		$modelVisa->addVisa($id_Analyse_images, $id_MS, $status_instructor); // 12
+		$modelVisa->addVisa($id_Cryostat, $id_MS, $status_instructor); // 13
+		$modelVisa->addVisa($id_Inclusion, $id_MS, $status_instructor); // 14
+		$modelVisa->addVisa($id_Loupe_binoculaire, $id_MS, $status_instructor); // 15
+		$modelVisa->addVisa($id_Microscope_nikon_NI_E, $id_MS, $status_instructor); // 16
+		$modelVisa->addVisa($id_Microtome, $id_MS, $status_instructor); // 17
+		$modelVisa->addVisa($id_Scanner_de_lames, $id_MS, $status_instructor); // 18
+		$modelVisa->addVisa($id_Snapfrost, $id_MS, $status_instructor); // 19
+		$modelVisa->addVisa($id_Vibratome, $id_MS, $status_instructor); // 20
+		$modelVisa->addVisa($id_Microscope_80i, $id_MS, $status_instructor); // 21
+		$modelVisa->addVisa($id_Intavis, $id_MS, $status_instructor); // 22
+		
+		$modelVisa->addVisa($id_Analyse_images, $id_PB, $status_instructor); // 23
+		$modelVisa->addVisa($id_Cryostat, $id_PB, $status_instructor); // 24
+		$modelVisa->addVisa($id_Inclusion, $id_PB, $status_instructor); // 25
+		$modelVisa->addVisa($id_Loupe_binoculaire, $id_PB, $status_instructor); // 26
+		$modelVisa->addVisa($id_Microscope_nikon_NI_E, $id_PB, $status_instructor); // 27
+		$modelVisa->addVisa($id_Microtome, $id_PB, $status_instructor); // 28
+		$modelVisa->addVisa($id_Scanner_de_lames, $id_PB, $status_instructor); // 29
+		$modelVisa->addVisa($id_Snapfrost, $id_PB, $status_instructor); // 30
+		$modelVisa->addVisa($id_Vibratome, $id_PB, $status_instructor); // 31
+		$modelVisa->addVisa($id_Microscope_80i, $id_PB, $status_instructor); // 32
+		$modelVisa->addVisa($id_Intavis, $id_PB, $status_instructor); // 33
+		
+		$modelVisa->addVisa($id_Analyse_images, $id_RV, $status_instructor); // 34
+		$modelVisa->addVisa($id_Cryostat, $id_RV, $status_instructor); // 35
+		$modelVisa->addVisa($id_Inclusion, $id_RV, $status_instructor); // 36
+		$modelVisa->addVisa($id_Loupe_binoculaire, $id_RV, $status_instructor); // 37
+		$modelVisa->addVisa($id_Microscope_nikon_NI_E, $id_RV, $status_instructor); // 38
+		$modelVisa->addVisa($id_Microtome, $id_RV, $status_instructor); // 39
+		$modelVisa->addVisa($id_Scanner_de_lames, $id_RV, $status_instructor); // 40
+		$modelVisa->addVisa($id_Snapfrost, $id_RV, $status_instructor); // 41
+		$modelVisa->addVisa($id_Vibratome, $id_RV, $status_instructor); // 42
+		$modelVisa->addVisa($id_Microscope_80i, $id_RV, $status_instructor); // 43
+		$modelVisa->addVisa($id_Intavis, $id_RV, $status_instructor); // 44
+		
+		$modelVisa->addVisa($id_Analyse_images, $id_GB, $status_instructor); // 45
+		$modelVisa->addVisa($id_Cryostat, $id_GB, $status_instructor); // 46
+		$modelVisa->addVisa($id_Inclusion, $id_GB, $status_instructor); // 47
+		$modelVisa->addVisa($id_Loupe_binoculaire, $id_GB, $status_instructor); // 48
+		$modelVisa->addVisa($id_Microscope_nikon_NI_E, $id_GB, $status_instructor); // 49
+		$modelVisa->addVisa($id_Microtome, $id_GB, $status_instructor); // 50
+		$modelVisa->addVisa($id_Scanner_de_lames, $id_GB, $status_instructor); // 51
+		$modelVisa->addVisa($id_Snapfrost, $id_GB, $status_instructor); // 52
+		$modelVisa->addVisa($id_Vibratome, $id_GB, $status_instructor); // 53
+		$modelVisa->addVisa($id_Microscope_80i, $id_GB, $status_instructor); // 54
+		$modelVisa->addVisa($id_Intavis, $id_GB, $status_instructor); // 55
+		
+		// edit authorizations
 		$modelAuthorizations = new SyAuthorization();
 		$auths = $modelAuthorizations->getAuths();
 		foreach($auths as $au){
-			$id = $au["id"]; 
-			$date = $au["date"]; 
+			$id = $au["id"];
+			$date = $au["date"];
 			$user_id = $au["user_id"];
-			$lab_id = $au["lab_id"]; 
-			$resource_id = $au["resource_id"]; 
+			$lab_id = $au["lab_id"];
+			$resource_id = $au["resource_id"];
+			$visa_id = $au["visa_id"];
 			
-			$modelAuthorizations->editAuthorization($id, $date, $user_id, $lab_id, 1, $resource_id);
+			
+			$visaID = 1;
+			if ($visa_id == $id_visa_AF){
+				$visaID = $modelVisa->getVisaFromResourceAndInstructor($resource_id, $id_AF);
+			}
+			if ($visa_id == $id_visa_MS){
+				$visaID = $modelVisa->getVisaFromResourceAndInstructor($resource_id, $id_MS);
+			}
+			if ($visa_id == $id_visa_PB){
+				$visaID = $modelVisa->getVisaFromResourceAndInstructor($resource_id, $id_PB);
+			}
+			if ($visa_id == $id_visa_RV){
+				$visaID = $modelVisa->getVisaFromResourceAndInstructor($resource_id, $id_RV);
+			}
+			if ($visa_id == $id_visa_GB){
+				$visaID = $modelVisa->getVisaFromResourceAndInstructor($resource_id, $id_GB);
+			}
+				
+			$modelAuthorizations->editAuthorization($id, $date, $user_id, $lab_id, $visaID, $resource_id);
 		}
 		
 		// 4- enlarge the header column of sy_bookingcss
@@ -151,8 +259,7 @@ class ControllerSynvnew extends Controller {
 			$text_color = "#" . $cc["text"];
 			$display_order = $cc["display_order"];
 			$moduleColorCode->editColorCode($id, $name, $color, $text_color, $display_order);
-		}
-*/		
+		}	
 		
 		// 6- remove useless tables and columns
 		echo "<p> remove useless tables and columns... </p>";
